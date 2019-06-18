94cf8fe65a681f2c7df75d619fca8c5490d10e4a
Ozan Gelmez <ozan-bulu-t@hotmail.com>
2019-06-18 12:45:42 +0300

    Created upload view,controller,model.

--- FileUploadDownload/FileUploadDownload/Controllers/HomeController.cs
+++ FileUploadDownload/FileUploadDownload/Controllers/HomeController.cs
@@ -5,25 +5,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FileUploadDownload.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace FileUploadDownload.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _enviroment;
+        public HomeController(IHostingEnvironment enviroment)
+        {
+            _enviroment = enviroment;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+     [HttpPost]
+     public IActionResult Upload(FileUploadDownloadViewModel model)
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var file = model.File;
+            if(file.Length>0)
+            {
+                string path = Path.Combine(_enviroment.WebRootPath, "images");
+                using (var fileStream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+                model.destination = $"/images{file.FileName}";
+                model.extension = Path.GetExtension(file.FileName).Substring(1);
+                return Ok(model);
+            }
+            return BadRequest();
         }
     }
 }
---
+++ FileUploadDownload/FileUploadDownload/Models/FileUploadDownloadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadDownload.Models
+{
+    public class FileUploadDownloadViewModel
+    {
+        public IFormFile File { get; set; }
+        public long size { get; set; }
+        public int width { get; set; }
+        public  int height{ get; set; }
+        public string extension { get; set; }
+        public string destination { get; set; }
+    }
+}
